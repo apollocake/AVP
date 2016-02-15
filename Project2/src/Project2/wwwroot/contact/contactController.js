0d4c581d8bfd17ba3482df744657ce694274e01f
Afton Martin <aftonam@gmail.com>
2016-02-15 08:34:43 +0000

    added contact me page

---
+++ Project2/src/Project2/wwwroot/contact/contactController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('project2')
+        .controller('contactController', contactController);
+
+    //contactController.$inject = ['$location']; 
+
+    function contactController($scope) {
+        // function to submit the form after all validation has occurred            
+        $scope.submitForm = function () {
+
+            // check to make sure the form is completely valid
+            if ($scope.userForm.$valid) {
+                alert('our form is amazing');
+            }
+
+        };
+    }
+})();
